e09edcb903bf4b7e99a364a976d3c9acd05fc307
atching <atching@users.noreply.github.com>
2014-01-18 17:54:04 -0800

    Fixed the SPI bit banging

--- spi_control.c
+++ spi_control.c
@@ -24,36 +24,37 @@ void spi_send(unsigned int address, unsigned int data){
 	int i = 0;
 
 	IOWR_ALTERA_AVALON_PIO_DATA(CS_BASE, 0); // Begin
-	delay(10);
+	delay(150);
 	for(i = 0; i < 7; i++){
-		if(address & 0x8000){
+		if(address & 0x40){
 			IOWR_ALTERA_AVALON_PIO_DATA(SDIN_BASE, 1);
 			//printf("1");
 		}else{
 			IOWR_ALTERA_AVALON_PIO_DATA(SDIN_BASE, 0);
 			//printf("0");
 		}
-		delay(10);
+		delay(150);
 		IOWR_ALTERA_AVALON_PIO_DATA(SCLK_BASE, 1);
-		delay(10);
+		delay(150);
 		IOWR_ALTERA_AVALON_PIO_DATA(SCLK_BASE, 0);
-		delay(10);
+		delay(150);
 
 		address <<= 1;
 	}
 
 	for(i = 0; i < 9; i++){
-		if(data & 0x8000){
+		if(data & 0x100){
 			IOWR_ALTERA_AVALON_PIO_DATA(SDIN_BASE, 1);
 			//printf("1");
 		}else{
 			IOWR_ALTERA_AVALON_PIO_DATA(SDIN_BASE, 0);
 			//printf("0");
 		}
+		delay(150);
 		IOWR_ALTERA_AVALON_PIO_DATA(SCLK_BASE, 1);
-		delay(10);
+		delay(150);
 		IOWR_ALTERA_AVALON_PIO_DATA(SCLK_BASE, 0);
-		delay(10);
+		delay(150);
 
 		data <<= 1;
 	}
